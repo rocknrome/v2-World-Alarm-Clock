dcab73a91bfc4509f66dadfa12b97a6f84f5cb75
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-05 20:06:56 -0500

    Index.html, App.js and Style.css files and functionality created

--- app.js
+++ app.js
@@ -1,85 +1,59 @@
-//   R'N'R   November 29, 2023
+// Declaring variables
+const $timezoneSelect = $('#timezone-select');
+const $alarmTimeInput = $('#alarm-time');
+const $setAlarmButton = $('#set-alarm');
+const $alarmDisplay = $('#alarm-display');
+let alarms = [];
 
-//---------------------------------------------------//
-//              APP JAVASCRIPT FILE                  //
-//---------------------------------------------------//
+// List of cities for the timezone dropdown
+const cities = ['London', 'Moscow', 'New York', 'Paris', 'Sydney', 'Tokyo'];
 
+// Function to populate the dropdown with cities
+function populateCityDropdown() {
+    $timezoneSelect.empty();
+    cities.forEach(city => {
+        $timezoneSelect.append('<option value="' + city + '">' + city + '</option>');
+    });
+}
+populateCityDropdown();
 
-    //---------------------------------------------------//
-    //Declaring variables
-    //---------------------------------------------------//
-    const $timezoneSelect = $('#timezone-select');
-    const $alarmTimeInput = $('#alarm-time');
-    const $setAlarmButton = $('#set-alarm');
-    const $alarmDisplay = $('#alarm-display');
-    let alarms = [];
+// Function to display alarms
+function displayAlarms() {
+    $alarmDisplay.empty();
+    alarms.forEach((alarm, index) => {
+        $alarmDisplay.append(`
+            <div class="alarm">
+                Alarm at ${alarm.time} in ${alarm.timezone}
+                <button onclick="editAlarm(${index})">Edit</button>
+                <button onclick="deleteAlarm(${index})">Delete</button>
+            </div>
+        `);
+    });
+}
 
+// Set an alarm
+$setAlarmButton.click(function() {
+    const city = $timezoneSelect.val();
+    const time = $alarmTimeInput.val();
+    // Here you can make an API call to fetch timezone data for the selected city
+    // For simplicity, using the city name as the timezone
+    alarms.push({ timezone: city, time: time });
+    displayAlarms();
+});
 
-    //---------------------------------------------------//
-    //Setting up functionality
-    //---------------------------------------------------//
-
-    // Fetch and populate timezones
-    function populateTimeZone() {
-        $.ajax({
-            url: 'http://worldtimeapi.org/api/timezone',
-            success: function(timezones) {
-                timezones.forEach(zone => {
-                    $timezoneSelect.append(`<option value="${zone}">${zone}</option>`);
-                });
-            },
-            error: function(error) {
-                console.log('Error fetching timezones:', error);
-            }
-        });
+// Edit an alarm
+window.editAlarm = function(index) {
+    const newTime = prompt("Enter new time (HH:MM format):", alarms[index].time);
+    if(newTime) {
+        alarms[index].time = newTime;
+        displayAlarms();
     }
-    populateTimeZone()
+};
 
+// Delete an alarm
+window.deleteAlarm = function(index) {
+    alarms.splice(index, 1);
+    displayAlarms();
+};
 
-    // Function to display alarms
-    function displayAlarms() {
-        $alarmDisplay.empty();  //clearing out the display block initially
-        alarms.forEach((alarm, index) => {
-          $alarmDisplay.append(`
-                    <div class="alarm">
-                        Alarm at ${alarm.time} in ${alarm.timezone}
-                        <button onclick="editAlarm(${index})">Edit</button>
-                        <button onclick="deleteAlarm(${index})">Delete</button>
-                    </div>
-                `);
-        });
-      }
-    //   displayAlarms()
-
-
-    // Set an alarm
-    $setAlarmButton.click(function() {  //event listener using the jQuery method ".click"
-        const timezone = $timezoneSelect.val();
-        const time = $alarmTimeInput.val();
-        alarms.push({ timezone, time });
-        displayAlarms();
-      });
-
-
-    // Edit an alarm
-        //edit functionality here
-        window.editAlarm = function(index) {
-            const newTime = prompt("Enter new time (HH:MM format):", alarms[index].time);   //declaring new variables for adjusted time
-                if(newTime) {
-                    alarms[index].time = newTime;
-                    displayAlarms();
-                }
-        };
-
-
-    // Delete an alarm
-    window.deleteAlarm = function(index) {  //declaring a global function thru "window"
-        alarms.splice(index, 1);    //removing specific alarm with a specific index, one element only
-        displayAlarms();
-    };
-
-
-    // Alarm triggering
-
-
-
+// Alarm triggering functionality to be implemented
